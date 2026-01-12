a745140bcff993796d67461cdeb10fc04b989f9f
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-12 15:23:16 +0530

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,15 +1,10 @@
 library = []
 
-# Add 3 books
-for i in range(1):
-    print(f"\nEnter details for Book ")
-    book_id = input("Book ID: ")
-    title = input("Title: ")
-    author = input("Author: ")
-    year = input("Year: ")
-    library.append([book_id, title, author, year])
+with open("books.txt", "r") as file:
+    for line in file:
+        data = line.strip().split(",")
+        library.append(data)
 
-# Display books
 print("\n--- Library Books ---")
 for b in library:
-    print(f"ID: {b[0]}, Title: {b[1]}")
+    print(f"ID: {b[0]}, Title: {b[1]}, Author: {b[2]}, Year: {b[3]}")
