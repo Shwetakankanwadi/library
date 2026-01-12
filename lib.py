984f1bb6cd46a921e91557fa734d64cbc83c296d
shwetakankanwadi <shwetakankanwadi878@gmail.com>
2026-01-12 14:58:04 +0530

    save

---
+++ lib.py
@@ -0,0 +1,15 @@
+library = []
+
+# Add 3 books
+for i in range(1):
+    print(f"\nEnter details for Book ")
+    book_id = input("Book ID: ")
+    title = input("Title: ")
+    author = input("Author: ")
+    year = input("Year: ")
+    library.append([book_id, title, author, year])
+
+# Display books
+print("\n--- Library Books ---")
+for b in library:
+    print(f"ID: {b[0]}, Title: {b[1]}")
